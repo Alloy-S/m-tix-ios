3398ba6d6926e2de64d28ca7932e1bc5b4f11430
Alloysius Steven Hartoyo <107180700+Alloy-S@users.noreply.github.com>
2023-12-15 22:30:45 +0700

    init

---
+++ PROYEK_IOS/PROYEK_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PROYEK_IOS
+//
+//  Created by Alloysius Steven on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
