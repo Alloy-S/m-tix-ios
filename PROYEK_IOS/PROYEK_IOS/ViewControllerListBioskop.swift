3cdd0054760f257e8dc3cf50fd3c1f553e5fbc21
Alloysius Steven Hartoyo <107180700+Alloy-S@users.noreply.github.com>
2023-12-17 20:28:23 +0700

    add filter bioskop

--- PROYEK_IOS/PROYEK_IOS/ViewControllerListBioskop.swift
+++ PROYEK_IOS/PROYEK_IOS/ViewControllerListBioskop.swift
@@ -8,36 +8,73 @@
 import UIKit
 import FirebaseFirestore
 
-class ViewControllerListBioskop: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewControllerListBioskop: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arBioskop.count
+        if filtered {
+            return filteredBioskop.count
+        } else {
+            return arBioskop.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellListBioskop")! as! TableViewCellListBioskop
-        cell.namaBioskop.text = arBioskop[indexPath.row].nama
-        
+        if filtered {
+            cell.namaBioskop.text = filteredBioskop[indexPath.row].nama
+        } else {
+            cell.namaBioskop.text = arBioskop[indexPath.row].nama
+        }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedItem = arBioskop[indexPath.row]
+        var selectedItem = Bioskop(bioskopId: "", alamat: "", nama: "", telp: "", movieId: [])
+        if filtered {
+            selectedItem = filteredBioskop[indexPath.row]
+        } else {
+            selectedItem = arBioskop[indexPath.row]
+        }
         let detailVC = ViewControllerListJadwal(nibName: "ViewControllerListJadwal", bundle: nil)
         detailVC.idBioskop = selectedItem.bioskopId
         navigationController?.pushViewController(detailVC, animated: true)
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = tfSearch.text {
+            filterData(text + string)
+        }
+        
+        return true
+    }
     
+    func filterData(_ query: String) {
+        filteredBioskop.removeAll()
+        for i in 0...arBioskop.count-1 {
+            if arBioskop[i].nama.lowercased().contains(query.lowercased()) {
+                filteredBioskop.append(arBioskop[i])
+            }
+        }
+        
+        if filteredBioskop.isEmpty {
+            filtered = false
+        }
+        tableView.reloadData()
+        filtered = true
+    }
+    
+    var filtered = false
     
     @IBAction func btnReload(_ sender: UIButton) {
         loadData()
     }
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var tfSearch: UITextField!
+    
     let db = Firestore.firestore()
     
     var arBioskop: [Bioskop] = []
-    
+    var filteredBioskop: [Bioskop] = []
     func loadData() {
         db.collection("Bioskop").getDocuments() { (querySnapshot, err)
             in
@@ -73,7 +110,7 @@ class ViewControllerListBioskop: UIViewController, UITableViewDelegate, UITableV
 //        tableView.dataSource = self
         
             // Do any additional setup after loading the view.
-            
+            self.tfSearch?.delegate = self
             loadData()
             
         
