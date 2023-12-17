9e91048aedf447c1698738db2ef58e4fcb067195
StevenArnoldS <c14210169@john.petra.ac.id>
2023-12-17 22:46:26 +0700

    nowPlaying & upComing

---
+++ PROYEK_IOS/PROYEK_IOS/nowPlaying.swift
@@ -0,0 +1,16 @@
+//
+//  nowPlaying.swift
+//  PROYEK_IOS
+//
+//  Created by Steven A on 17/12/23.
+//
+
+import Foundation
+import UIKit
+
+class nowPlaying: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ PROYEK_IOS/PROYEK_IOS/upComing.swift
@@ -0,0 +1,16 @@
+//
+//  upComing.swift
+//  PROYEK_IOS
+//
+//  Created by Steven A on 17/12/23.
+//
+
+import Foundation
+import UIKit
+
+class upComing: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
