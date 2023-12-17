8ff5110d69df82f70e2f25c0385893f6b63348f3
Alloysius Steven Hartoyo <107180700+Alloy-S@users.noreply.github.com>
2023-12-17 15:24:46 +0700

    add navigation bar

--- PROYEK_IOS/PROYEK_IOS/ViewControllerListBioskop.swift
+++ PROYEK_IOS/PROYEK_IOS/ViewControllerListBioskop.swift
@@ -6,24 +6,40 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewControllerListBioskop: UIViewController {
-
+    
+    
+    let db = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        db.collection("Bioskop").getDocuments() { (querySnapshot, err)
+            in
+            if let err = err
+            {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print(document.data())
+                }
+                
+            }
+            // Do any additional setup after loading the view.
+            
+            
+            
+            /*
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
