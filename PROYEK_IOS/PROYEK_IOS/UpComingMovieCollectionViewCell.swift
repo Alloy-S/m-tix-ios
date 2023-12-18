a855f658978d3921ae4aeabc2962815c048ed69a
StevenArnoldS <c14210169@john.petra.ac.id>
2023-12-18 13:47:14 +0700

    upcoming

---
+++ PROYEK_IOS/PROYEK_IOS/UpComingMovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UpComingMovieCollectionViewCell.swift
+//  PROYEK_IOS
+//
+//  Created by Steven A on 18/12/23.
+//
+
+import UIKit
+
+class UpComingMovieCollectionViewCell: UICollectionViewCell {
+    
+}
